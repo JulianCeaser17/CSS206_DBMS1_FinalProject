79e4cf043aa2efa98c809446875c27e2e53667da
JulianCeasar17 <seitkulov18@gmail.com>
2022-12-17 23:59:33 +0600

    languages

--- insert_languages.sql
+++ insert_languages.sql
@@ -1,102 +1,201 @@
-insert into LANGUAGES (LANGUAGE_NAME) 
-SELECT 'Af', 
-SELECT 'Fr', 
-SELECT 'Bo', 
-SELECT 'Ma', 
-SELECT 'La', 
-SELECT 'Pa', 
-SELECT 'Cr', 
-SELECT 'Th', 
-SELECT 'Ga', 
-SELECT 'Ka', 
-SELECT 'Te', 
-SELECT 'Mo', 
-SELECT 'Ir', 
-SELECT 'Ma', 
-SELECT 'Ka', 
-SELECT 'Da', 
-SELECT 'Fi', 
-SELECT 'Bu', 
-SELECT 'Bu', 
-SELECT 'Ne', 
-SELECT 'Ay', 
-SELECT 'Pu', 
-SELECT 'To', 
-SELECT 'No', 
-SELECT 'Kh', 
-SELECT 'No', 
-SELECT 'Nd', 
-SELECT 'To', 
-SELECT 'Ja', 
-SELECT 'Bu', 
-SELECT 'Pa', 
-SELECT 'Pa', 
-SELECT 'Fi', 
-SELECT 'Ne', 
-SELECT 'Ha', 
-SELECT 'Or', 
-SELECT 'Hi', 
-SELECT 'Gu', 
-SELECT 'Po', 
-SELECT 'Mo', 
-SELECT 'Or', 
-SELECT 'Fi', 
-SELECT 'Gu', 
-SELECT 'Te', 
-SELECT 'Gr', 
-SELECT 'Li', 
-SELECT 'Mo', 
-SELECT 'Ta', 
-SELECT 'Sw', 
-SELECT 'Sw', 
-SELECT 'It', 
-SELECT 'Be', 
-SELECT 'Pe', 
-SELECT 'Ky', 
-SELECT 'Li', 
-SELECT 'Ko', 
-SELECT 'Ma', 
-SELECT 'Ka', 
-SELECT 'He', 
-SELECT 'Az', 
-SELECT 'Ic', 
-SELECT 'Pa', 
-SELECT 'Gu', 
-SELECT 'Bi', 
-SELECT 'Nd', 
-SELECT 'Kh', 
-SELECT 'Ic', 
-SELECT 'Da', 
-SELECT 'Mo', 
-SELECT 'La', 
-SELECT 'Po', 
-SELECT 'Mo', 
-SELECT 'Yi', 
-SELECT 'To', 
-SELECT 'Cz', 
-SELECT 'Lu', 
-SELECT 'Cz', 
-SELECT 'Bu', 
-SELECT 'Ko', 
-SELECT 'Du', 
-SELECT 'Ro', 
-SELECT 'Sp', 
-SELECT 'As', 
-SELECT 'Da', 
-SELECT 'So', 
-SELECT 'Ta', 
-SELECT 'Po', 
-SELECT 'Ku', 
-SELECT 'Qu', 
-SELECT 'Hu', 
-SELECT 'Ne', 
-SELECT 'Sp', 
-SELECT 'We', 
-SELECT 'Po', 
-SELECT 'En', 
-SELECT 'Pa', 
-SELECT 'Cz', 
-SELECT 'Th', 
-SELECT 'Hi', 
-SELECT 'Ka', 
+insert into LANGUAGES (LANGUAGE_CODE, LANGUAGE_NAME) 
+SELECT 'Af', 'Afrikaans' from dual union all
+SELECT 'Fr', 'French' from dual union all
+SELECT 'Bo', 'Bosnian' from dual union all
+SELECT 'Ma', 'Malagasy' from dual union all
+SELECT 'La', 'Latvian' from dual union all
+SELECT 'Pa', 'Pashto' from dual union all
+SELECT 'Cr', 'Croatian' from dual union all
+SELECT 'Th', 'Thai' from dual union all
+SELECT 'Ga', 'Gagauz' from dual union all
+SELECT 'Ka', 'Kashmiri' from dual union all
+SELECT 'Te', 'Telugu' from dual union all
+SELECT 'Mo', 'Moldovan' from dual union all
+SELECT 'Ir', 'Irish Gaelic' from dual union all
+SELECT 'Ma', 'Malay' from dual union all
+SELECT 'Ka', 'Kannada' from dual union all
+SELECT 'Da', 'Danish' from dual union all
+SELECT 'Fi', 'Fijian' from dual union all
+SELECT 'Bu', 'Burmese' from dual union all
+SELECT 'Bu', 'Bulgarian' from dual union all
+SELECT 'Ne', 'Nepali' from dual union all
+SELECT 'Ay', 'Aymara' from dual union all
+SELECT 'Pu', 'Punjabi' from dual union all
+SELECT 'To', 'Tok Pisin' from dual union all
+SELECT 'No', 'Norwegian' from dual union all
+SELECT 'Kh', 'Khmer' from dual union all
+SELECT 'No', 'Northern Sotho' from dual union all
+SELECT 'Nd', 'Ndebele' from dual union all
+SELECT 'To', 'Tok Pisin' from dual union all
+SELECT 'Ja', 'Japanese' from dual union all
+SELECT 'Bu', 'Burmese' from dual union all
+SELECT 'Pa', 'Pashto' from dual union all
+SELECT 'Pa', 'Pashto' from dual union all
+SELECT 'Fi', 'Finnish' from dual union all
+SELECT 'Ne', 'Nepali' from dual union all
+SELECT 'Ha', 'Haitian Creole' from dual union all
+SELECT 'Or', 'Oriya' from dual union all
+SELECT 'Hi', 'Hindi' from dual union all
+SELECT 'Gu', 'Guaraní' from dual union all
+SELECT 'Po', 'Polish' from dual union all
+SELECT 'Mo', 'Moldovan' from dual union all
+SELECT 'Or', 'Oriya' from dual union all
+SELECT 'Fi', 'Filipino' from dual union all
+SELECT 'Gu', 'Gujarati' from dual union all
+SELECT 'Te', 'Tetum' from dual union all
+SELECT 'Gr', 'Greek' from dual union all
+SELECT 'Li', 'Lithuanian' from dual union all
+SELECT 'Mo', 'Mongolian' from dual union all
+SELECT 'Ta', 'Tajik' from dual union all
+SELECT 'Sw', 'Swedish' from dual union all
+SELECT 'Sw', 'Swati' from dual union all
+SELECT 'It', 'Italian' from dual union all
+SELECT 'Be', 'Belarusian' from dual union all
+SELECT 'Pe', 'Persian' from dual union all
+SELECT 'Ky', 'Kyrgyz' from dual union all
+SELECT 'Li', 'Lithuanian' from dual union all
+SELECT 'Ko', 'Korean' from dual union all
+SELECT 'Ma', 'Macedonian' from dual union all
+SELECT 'Ka', 'Kashmiri' from dual union all
+SELECT 'He', 'Hebrew' from dual union all
+SELECT 'Az', 'Azeri' from dual union all
+SELECT 'Ic', 'Icelandic' from dual union all
+SELECT 'Pa', 'Papiamento' from dual union all
+SELECT 'Gu', 'Guaraní' from dual union all
+SELECT 'Bi', 'Bislama' from dual union all
+SELECT 'Nd', 'Ndebele' from dual union all
+SELECT 'Kh', 'Khmer' from dual union all
+SELECT 'Ic', 'Icelandic' from dual union all
+SELECT 'Da', 'Dari' from dual union all
+SELECT 'Mo', 'Mongolian' from dual union all
+SELECT 'La', 'Latvian' from dual union all
+SELECT 'Po', 'Portuguese' from dual union all
+SELECT 'Mo', 'Montenegrin' from dual union all
+SELECT 'Yi', 'Yiddish' from dual union all
+SELECT 'To', 'Tok Pisin' from dual union all
+SELECT 'Cz', 'Czech' from dual union all
+SELECT 'Lu', 'Luxembourgish' from dual union all
+SELECT 'Cz', 'Czech' from dual union all
+SELECT 'Bu', 'Bulgarian' from dual union all
+SELECT 'Ko', 'Korean' from dual union all
+SELECT 'Du', 'Dutch' from dual union all
+SELECT 'Ro', 'Romanian' from dual union all
+SELECT 'Sp', 'Spanish' from dual union all
+SELECT 'As', 'Assamese' from dual union all
+SELECT 'Da', 'Dari' from dual union all
+SELECT 'So', 'Somali' from dual union all
+SELECT 'Ta', 'Tajik' from dual union all
+SELECT 'Po', 'Polish' from dual union all
+SELECT 'Ku', 'Kurdish' from dual union all
+SELECT 'Qu', 'Quechua' from dual union all
+SELECT 'Hu', 'Hungarian' from dual union all
+SELECT 'Ne', 'New Zealand Sign Language' from dual union all
+SELECT 'Sp', 'Spanish' from dual union all
+SELECT 'We', 'West Frisian' from dual union all
+SELECT 'Po', 'Polish' from dual union all
+SELECT 'En', 'English' from dual union all
+SELECT 'Pa', 'Pashto' from dual union all
+SELECT 'Cz', 'Czech' from dual union all
+SELECT 'Th', 'Thai' from dual union all
+SELECT 'Hi', 'Hiri Motu' from dual union all
+SELECT 'Ka', 'Kashmiri' from dual;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
