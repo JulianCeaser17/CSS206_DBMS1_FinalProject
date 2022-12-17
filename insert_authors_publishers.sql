670859b70ef4c62a2172923b4ed9e525cdfb54c3
JulianCeasar17 <seitkulov18@gmail.com>
2022-12-17 22:33:44 +0600

    insert authors and publishers

---
+++ insert_authors_publishers.sql
@@ -0,0 +1,204 @@
+insert into authors (author_name, author_lastname, author_email)
+select 'Minetta', 'Vedyasov', 'mvedyasov0@discuz.net' from dual union all
+select 'Jori', 'Tottle', 'jtottle1@ow.ly' from dual union all
+select 'Sapphire', 'Larchier', 'slarchier2@live.com' from dual union all
+select 'Marc', 'Vautre', 'mvautre3@xing.com' from dual union all
+select 'Gal', 'Renny', 'grenny4@nba.com' from dual union all
+select 'Margie', 'Whettleton', 'mwhettleton5@usatoday.com' from dual union all
+select 'Giuditta', 'Jouannot', 'gjouannot6@squarespace.com' from dual union all
+select 'Arlyne', 'Coalburn', 'acoalburn7@delicious.com' from dual union all
+select 'Marie', 'Innman', 'minnman8@mozilla.org' from dual union all
+select 'Tait', 'Isaksson', 'tisaksson9@sciencedaily.com' from dual union all
+select 'Corenda', 'Pigeon', 'cpigeona@de.vu' from dual union all
+select 'Jana', 'Furse', 'jfurseb@4shared.com' from dual union all
+select 'Stefano', 'Learned', 'slearnedc@berkeley.edu' from dual union all
+select 'Stefan', 'Sowte', 'ssowted@blog.com' from dual union all
+select 'Judas', 'Bresner', 'jbresnere@army.mil' from dual union all
+select 'Angeli', 'Philipet', 'aphilipetf@bluehost.com' from dual union all
+select 'Jocko', 'Schroeder', 'jschroederg@businessinsider.com' from dual union all
+select 'Lucien', 'Brister', 'lbristerh@rakuten.co.jp' from dual union all
+select 'Darb', 'Larsen', 'dlarseni@forbes.com' from dual union all
+select 'Melinde', 'Mattusov', 'mmattusovj@hubpages.com' from dual union all
+select 'Oona', 'McShirrie', 'omcshirriek@un.org' from dual union all
+select 'Carson', 'Foulcher', 'cfoulcherl@list-manage.com' from dual union all
+select 'Arch', 'Scholler', 'aschollerm@cdbaby.com' from dual union all
+select 'Fanchon', 'Sebrens', 'fsebrensn@delicious.com' from dual union all
+select 'Jade', 'Matteoni', 'jmatteonio@w3.org' from dual union all
+select 'Joya', 'Crichten', 'jcrichtenp@cmu.edu' from dual union all
+select 'Ethel', 'Cowope', 'ecowopeq@ezinearticles.com' from dual union all
+select 'Packston', 'Penhale', 'ppenhaler@admin.ch' from dual union all
+select 'Mylo', 'Hatje', 'mhatjes@meetup.com' from dual union all
+select 'Bevon', 'Bastide', 'bbastidet@weather.com' from dual union all
+select 'Angus', 'Sergeaunt', 'asergeauntu@noaa.gov' from dual union all
+select 'Roman', 'Lorkins', 'rlorkinsv@artisteer.com' from dual union all
+select 'Giana', 'Clinch', 'gclinchw@youtu.be' from dual union all
+select 'Drusi', 'Swinden', 'dswindenx@liveinternet.ru' from dual union all
+select 'Charisse', 'Eastabrook', 'ceastabrooky@youtu.be' from dual union all
+select 'Beverlee', 'Erwin', 'berwinz@cisco.com' from dual union all
+select 'Alf', 'Spofforth', 'aspofforth10@upenn.edu' from dual union all
+select 'Benoit', 'Satteford', 'bsatteford11@arstechnica.com' from dual union all
+select 'Jess', 'Cardenas', 'jcardenas12@dailymotion.com' from dual union all
+select 'Elvera', 'Cracoe', 'ecracoe13@bbc.co.uk' from dual union all
+select 'Ivory', 'Lazell', 'ilazell14@ucoz.ru' from dual union all
+select 'Othilia', 'Klosges', 'oklosges15@printfriendly.com' from dual union all
+select 'Gerald', 'Schimke', 'gschimke16@independent.co.uk' from dual union all
+select 'Lincoln', 'Voff', 'lvoff17@uol.com.br' from dual union all
+select 'Kellen', 'Leinweber', 'kleinweber18@jalbum.net' from dual union all
+select 'Tammi', 'Dorber', 'tdorber19@answers.com' from dual union all
+select 'Querida', 'MacUchadair', 'qmacuchadair1a@usnews.com' from dual union all
+select 'Mahmoud', 'Sterry', 'msterry1b@wisc.edu' from dual union all
+select 'Nixie', 'Schanke', 'nschanke1c@wunderground.com' from dual union all
+select 'Jenifer', 'Tewkesbury', 'jtewkesbury1d@bloglovin.com' from dual union all
+select 'Kristoforo', 'Shelvey', 'kshelvey1e@about.com' from dual union all
+select 'Thekla', 'Pevsner', 'tpevsner1f@tumblr.com' from dual union all
+select 'Alejandrina', 'Otham', 'aotham1g@sciencedirect.com' from dual union all
+select 'Jamaal', 'Giller', 'jgiller1h@si.edu' from dual union all
+select 'Dino', 'Fernyhough', 'dfernyhough1i@nature.com' from dual union all
+select 'Jeni', 'Treweke', 'jtreweke1j@timesonline.co.uk' from dual union all
+select 'Cash', 'L'' Estrange', 'clestrange1k@admin.ch' from dual union all
+select 'Woodman', 'Cosslett', 'wcosslett1l@google.de' from dual union all
+select 'Heinrik', 'Asbury', 'hasbury1m@sohu.com' from dual union all
+select 'Hetti', 'Addington', 'haddington1n@posterous.com' from dual union all
+select 'Frazier', 'Lavery', 'flavery1o@themeforest.net' from dual union all
+select 'Edee', 'Tewes', 'etewes1p@craigslist.org' from dual union all
+select 'Shelby', 'Petrou', 'spetrou1q@ovh.net' from dual union all
+select 'Denney', 'Fylan', 'dfylan1r@goo.ne.jp' from dual union all
+select 'Nev', 'Costain', 'ncostain1s@blog.com' from dual union all
+select 'Irvine', 'Lynskey', 'ilynskey1t@chicagotribune.com' from dual union all
+select 'Kristina', 'O''Codihie', 'kocodihie1u@simplemachines.org' from dual union all
+select 'Samaria', 'Towriss', 'stowriss1v@trellian.com' from dual union all
+select 'Eden', 'Lademann', 'elademann1w@domainmarket.com' from dual union all
+select 'Anabella', 'Chinn', 'achinn1x@furl.net' from dual union all
+select 'Gwennie', 'Bricksey', 'gbricksey1y@dot.gov' from dual union all
+select 'Bay', 'Durnill', 'bdurnill1z@furl.net' from dual union all
+select 'Janna', 'Tourry', 'jtourry20@wikia.com' from dual union all
+select 'Ulrick', 'Blodgett', 'ublodgett21@dell.com' from dual union all
+select 'Joice', 'Waumsley', 'jwaumsley22@apache.org' from dual union all
+select 'Miguela', 'Bampford', 'mbampford23@opensource.org' from dual union all
+select 'Nikki', 'Lepere', 'nlepere24@blinklist.com' from dual union all
+select 'Elmira', 'Spoward', 'espoward25@zimbio.com' from dual union all
+select 'Michel', 'Whitehair', 'mwhitehair26@disqus.com' from dual union all
+select 'Ede', 'Middiff', 'emiddiff27@biglobe.ne.jp' from dual union all
+select 'Lalo', 'Tapsell', 'ltapsell28@umn.edu' from dual union all
+select 'Neddie', 'Awton', 'nawton29@msn.com' from dual union all
+select 'Roscoe', 'Gosselin', 'rgosselin2a@usatoday.com' from dual union all
+select 'Dan', 'Boundley', 'dboundley2b@goo.gl' from dual union all
+select 'Calvin', 'McNickle', 'cmcnickle2c@constantcontact.com' from dual union all
+select 'Barrie', 'Bullivent', 'bbullivent2d@diigo.com' from dual union all
+select 'Sula', 'Cess', 'scess2e@google.com.au' from dual union all
+select 'Orin', 'Spaice', 'ospaice2f@wordpress.org' from dual union all
+select 'Galvin', 'Parken', 'gparken2g@blinklist.com' from dual union all
+select 'Claudina', 'Maffei', 'cmaffei2h@yahoo.co.jp' from dual union all
+select 'Shermy', 'Trammel', 'strammel2i@livejournal.com' from dual union all
+select 'Norean', 'Sankey', 'nsankey2j@seesaa.net' from dual union all
+select 'Alica', 'Keers', 'akeers2k@4shared.com' from dual union all
+select 'Kial', 'Claremont', 'kclaremont2l@bloglines.com' from dual union all
+select 'Vinita', 'Dewbury', 'vdewbury2m@auda.org.au' from dual union all
+select 'Merissa', 'Yaldren', 'myaldren2n@telegraph.co.uk' from dual union all
+select 'Mic', 'Halgarth', 'mhalgarth2o@pinterest.com' from dual union all
+select 'Raviv', 'Satchel', 'rsatchel2p@weibo.com' from dual union all
+select 'Innis', 'Bairstow', 'ibairstow2q@ameblo.jp' from dual union all
+select 'Garrik', 'Stickford', 'gstickford2r@free.fr' from dual;
+
+
+insert into PUBLISHERS (PUBLISHER_NAME, PUBLISHER_ADDRESS, PUBLISHER_PHONE, PUBLISHER_EMAIL) 
+select 'Vidoo', '3001 Duke Trail', '375 551 2843', 'jpomphrett0@economist.com' from dual union all
+select 'Voonix', '22548 Kingsford Circle', '935 462 5478', 'laidler1@github.com' from dual union all
+select 'Brightdog', '2634 Summit Road', '484 903 4859', 'dventham2@umn.edu' from dual union all
+select 'Edgeclub', '222 Atwood Circle', '449 185 7072', 'tbabington3@google.cn' from dual union all
+select 'Voonix', '567 Beilfuss Plaza', '957 251 2524', 'sbridie4@noaa.gov' from dual union all
+select 'Fadeo', '68630 Summerview Terrace', '471 837 2179', 'cblackster5@ibm.com' from dual union all
+select 'Skippad', '715 Valley Edge Avenue', '480 980 0590', 'rellis6@army.mil' from dual union all
+select 'Npath', '61 Kennedy Court', '338 144 1393', 'acater7@deliciousdays.com' from dual union all
+select 'Oyoloo', '98172 Starling Crossing', '161 614 1059', 'rdominico8@i2i.jp' from dual union all
+select 'Fatz', '766 Welch Terrace', '450 860 6133', 'ksummers9@simplemachines.org' from dual union all
+select 'Aivee', '90 Cordelia Point', '790 443 8712', 'rlingea@freewebs.com' from dual union all
+select 'Buzzdog', '517 Moland Street', '191 367 9179', 'tmeechanb@i2i.jp' from dual union all
+select 'Edgewire', '16 Express Alley', '619 829 2688', 'tartusc@is.gd' from dual union all
+select 'Shuffledrive', '9 Jay Parkway', '792 857 7649', 'lelamd@facebook.com' from dual union all
+select 'Gabcube', '20 Claremont Plaza', '438 304 3381', 'eserrailliere@pcworld.com' from dual union all
+select 'Twimm', '6863 Twin Pines Place', '744 894 5411', 'dsemarkf@bigcartel.com' from dual union all
+select 'Avavee', '539 Village Hill', '643 629 1008', 'rpalmarg@spiegel.de' from dual union all
+select 'Blogtags', '2919 Golf Drive', '748 592 1326', 'ahavickh@xing.com' from dual union all
+select 'Twimbo', '32 Ramsey Trail', '457 717 5571', 'lkibbei@phoca.cz' from dual union all
+select 'Dablist', '2428 Burrows Point', '968 586 5449', 'lcostelloj@bbc.co.uk' from dual union all
+select 'Linktype', '065 Nobel Alley', '894 156 8269', 'bgravenellk@npr.org' from dual union all
+select 'Livefish', '9953 Bashford Junction', '706 819 2471', 'aedgellerl@tuttocitta.it' from dual union all
+select 'Photolist', '4 Larry Hill', '960 612 2065', 'arawesm@mapy.cz' from dual union all
+select 'Twitterwire', '36480 Michigan Avenue', '948 745 3620', 'vmacgiffinn@delicious.com' from dual union all
+select 'Skyble', '48 Sommers Junction', '926 528 3844', 'amacmichaelo@cpanel.net' from dual union all
+select 'Realbridge', '6 Dryden Junction', '254 569 3394', 'jdonovanp@sakura.ne.jp' from dual union all
+select 'Youopia', '15832 Pearson Terrace', '797 630 3335', 'emacsweenq@sphinn.com' from dual union all
+select 'Yakidoo', '113 Aberg Court', '431 917 7222', 'ograyshanr@bing.com' from dual union all
+select 'Meejo', '52 Golf Course Center', '743 942 4997', 'ggueinns@blinklist.com' from dual union all
+select 'Miboo', '33 Hermina Circle', '312 232 8066', 'iforseitht@godaddy.com' from dual union all
+select 'Voonyx', '5 Declaration Pass', '496 349 4195', 'ccawkillu@wufoo.com' from dual union all
+select 'Jaxnation', '7 Saint Paul Park', '337 790 0795', 'edowsev@state.tx.us' from dual union all
+select 'Wikizz', '46 2nd Avenue', '162 891 4293', 'adabrowskiw@zimbio.com' from dual union all
+select 'Pixoboo', '336 Eagan Park', '627 798 0294', 'kpantridgex@unc.edu' from dual union all
+select 'Tagcat', '500 Pawling Drive', '476 599 8109', 'bgowingy@msu.edu' from dual union all
+select 'Devshare', '621 Mccormick Street', '539 180 8598', 'dcolumbinez@msn.com' from dual union all
+select 'Cogilith', '21 Spaight Alley', '383 692 6773', 'kboscher10@fastcompany.com' from dual union all
+select 'Quinu', '9 Hermina Parkway', '134 660 7484', 'mstiling11@yandex.ru' from dual union all
+select 'Tagcat', '68851 Manitowish Center', '672 466 0422', 'aroutham12@utexas.edu' from dual union all
+select 'Lajo', '17 Doe Crossing Terrace', '611 594 1669', 'thinners13@joomla.org' from dual union all
+select 'Aibox', '145 Vahlen Lane', '979 804 3928', 'gbentham14@yellowbook.com' from dual union all
+select 'Demizz', '4625 Spaight Center', '600 883 1735', 'nglover15@dell.com' from dual union all
+select 'Dynabox', '8 Ohio Point', '910 410 4805', 'rbrideoke16@deviantart.com' from dual union all
+select 'Quinu', '8 Lyons Terrace', '709 583 9252', 'binglefield17@ycombinator.com' from dual union all
+select 'Youbridge', '88851 Dryden Street', '833 418 9213', 'cthorneywork18@google.com.hk' from dual union all
+select 'Miboo', '2356 Browning Plaza', '276 313 5211', 'mbrightling19@cbsnews.com' from dual union all
+select 'Voonder', '8 Blaine Pass', '308 715 0863', 'mdevil1a@bloomberg.com' from dual union all
+select 'Babbleblab', '239 Pierstorff Pass', '506 182 9558', 'jblinkhorn1b@businessinsider.com' from dual union all
+select 'Minyx', '7 Annamark Lane', '983 285 7114', 'lreveley1c@deliciousdays.com' from dual union all
+select 'Ntags', '3292 Artisan Road', '860 387 5696', 'istallybrass1d@cbc.ca' from dual union all
+select 'Wordpedia', '98 Bunting Court', '386 414 6838', 'mbibey1e@ucoz.ru' from dual union all
+select 'Yodoo', '4 Union Pass', '360 951 4360', 'bschiersch1f@hostgator.com' from dual union all
+select 'Voolith', '727 John Wall Crossing', '614 438 8864', 'itomczak1g@twitter.com' from dual union all
+select 'Livetube', '0 Bayside Crossing', '570 765 8625', 'gfawson1h@netlog.com' from dual union all
+select 'Kwimbee', '59972 Manley Trail', '492 210 9444', 'estealey1i@ning.com' from dual union all
+select 'Trilith', '2 Garrison Road', '697 394 0784', 'dspradbrow1j@google.co.uk' from dual union all
+select 'Flipstorm', '722 Lukken Junction', '717 999 3899', 'nzimmer1k@bing.com' from dual union all
+select 'Realbridge', '086 Pennsylvania Point', '701 387 0492', 'bjeandillou1l@nasa.gov' from dual union all
+select 'Quaxo', '32 Esker Junction', '444 997 5534', 'bluciano1m@drupal.org' from dual union all
+select 'Flashpoint', '7 Village Crossing', '333 125 5400', 'mthicking1n@answers.com' from dual union all
+select 'Latz', '5076 Eggendart Park', '497 790 1197', 'larnow1o@yale.edu' from dual union all
+select 'Photojam', '82062 Knutson Parkway', '863 800 4872', 'vfearey1p@csmonitor.com' from dual union all
+select 'Skipstorm', '470 Iowa Pass', '815 101 7925', 'bjosovitz1q@unicef.org' from dual union all
+select 'Twitterworks', '12 Walton Place', '487 462 1605', 'gcholton1r@xrea.com' from dual union all
+select 'Eayo', '26 Norway Maple Place', '510 198 8625', 'csteanyng1s@facebook.com' from dual union all
+select 'Tagpad', '711 Starling Pass', '168 581 3093', 'idurrad1t@ted.com' from dual union all
+select 'Twitternation', '16161 Lakeland Crossing', '130 432 9894', 'drodd1u@miitbeian.gov.cn' from dual union all
+select 'Oloo', '8 Little Fleur Trail', '308 733 2879', 'bgoodings1v@1688.com' from dual union all
+select 'Brainbox', '1730 Clarendon Crossing', '368 516 3601', 'klaye1w@so-net.ne.jp' from dual union all
+select 'Jetwire', '1 Rusk Court', '630 126 0065', 'acauley1x@mlb.com' from dual union all
+select 'JumpXS', '7141 Hoepker Hill', '899 846 8668', 'elepope1y@dell.com' from dual union all
+select 'Shuffledrive', '34914 Maryland Lane', '819 477 7703', 'bjozefowicz1z@state.tx.us' from dual union all
+select 'Ailane', '14261 Grayhawk Way', '403 141 3912', 'kkasman20@topsy.com' from dual union all
+select 'Gabtype', '9 Sycamore Avenue', '382 788 7046', 'rkiltie21@theglobeandmail.com' from dual union all
+select 'Bluezoom', '1 Pierstorff Circle', '915 871 2505', 'pcherryman22@mtv.com' from dual union all
+select 'Jaxspan', '6 Granby Parkway', '567 693 1940', 'rmacneill23@1688.com' from dual union all
+select 'Vitz', '66461 Pankratz Drive', '782 846 2099', 'hthow24@upenn.edu' from dual union all
+select 'Kazio', '677 Bluestem Circle', '395 900 0134', 'aopdenorth25@abc.net.au' from dual union all
+select 'Muxo', '51 High Crossing Avenue', '809 158 2829', 'msteutly26@huffingtonpost.com' from dual union all
+select 'Zoovu', '6 Reindahl Alley', '918 498 8916', 'kpurdon27@acquirethisname.com' from dual union all
+select 'Quinu', '3432 American Terrace', '834 753 8976', 'fculley28@sun.com' from dual union all
+select 'Feedmix', '3 Delaware Alley', '900 873 5217', 'dgounin29@phpbb.com' from dual union all
+select 'Rhyloo', '50940 Manufacturers Lane', '152 894 9013', 'hskarin2a@squarespace.com' from dual union all
+select 'Thoughtworks', '2100 Lawn Point', '532 358 5984', 'bpavlitschek2b@ucoz.com' from dual union all
+select 'Camido', '13 Surrey Road', '300 300 7945', 'tcastanho2c@tinypic.com' from dual union all
+select 'Fivespan', '49 Vidon Point', '928 696 8209', 'bebbett2d@deliciousdays.com' from dual union all
+select 'Voolith', '93753 Elmside Place', '969 322 0676', 'wmaginot2e@dailymotion.com' from dual union all
+select 'Livepath', '506 Lyons Court', '248 498 1386', 'emaceveley2f@diigo.com' from dual union all
+select 'Fadeo', '0220 Sherman Circle', '629 583 9258', 'hharman2g@loc.gov' from dual union all
+select 'Photobug', '1930 Tennessee Way', '692 749 4184', 'nfranken2h@europa.eu' from dual union all
+select 'Meevee', '4985 Linden Park', '491 511 2421', 'glanktree2i@privacy.gov.au' from dual union all
+select 'Yabox', '69206 La Follette Circle', '578 332 2198', 'dspinke2j@meetup.com' from dual union all
+select 'Meedoo', '0 Sycamore Crossing', '618 293 5832', 'tbreem2k@squarespace.com' from dual union all
+select 'Tagcat', '4330 Granby Hill', '975 434 8567', 'jvolet2l@telegraph.co.uk' from dual union all
+select 'Wikibox', '63147 Express Crossing', '468 741 3552', 'asenten2m@skyrock.com' from dual union all
+select 'Browsecat', '13 Goodland Terrace', '843 291 7864', 'tcrosskill2n@sitemeter.com' from dual union all
+select 'Skidoo', '973 Barby Center', '384 377 1527', 'breimer2o@adobe.com' from dual union all
+select 'Oyonder', '47074 American Circle', '303 503 9990', 'ehussey2p@meetup.com' from dual union all
+select 'Browsedrive', '6981 Crownhardt Way', '482 783 5018', 'fcovill2q@plala.or.jp' from dual union all
+select 'Tavu', '002 Hoepker Terrace', '331 288 8025', 'ycrayke2r@google.ca' from dual;
