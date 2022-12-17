c7d76db49b9839688e4f67e5b6bb15b342a0097e
JulianCeasar17 <seitkulov18@gmail.com>
2022-12-17 23:21:37 +0600

    insert departments and employees

---
+++ insert_departments_employees.sql
@@ -0,0 +1,204 @@
+insert into DEPARTMENTS (DEPARTMENT_NAME) 
+select 'Legal' from dual union all
+select 'Product Management' from dual union all
+select 'Services' from dual union all
+select 'Research and Development' from dual union all
+select 'Training' from dual union all
+select 'Legal' from dual union all
+select 'Sales' from dual union all
+select 'Legal' from dual union all
+select 'Research and Development' from dual union all
+select 'Human Resources' from dual union all
+select 'Training' from dual union all
+select 'Legal' from dual union all
+select 'Accounting' from dual union all
+select 'Marketing' from dual union all
+select 'Engineering' from dual union all
+select 'Product Management' from dual union all
+select 'Services' from dual union all
+select 'Engineering' from dual union all
+select 'Accounting' from dual union all
+select 'Engineering' from dual union all
+select 'Engineering' from dual union all
+select 'Services' from dual union all
+select 'Product Management' from dual union all
+select 'Services' from dual union all
+select 'Human Resources' from dual union all
+select 'Sales' from dual union all
+select 'Engineering' from dual union all
+select 'Legal' from dual union all
+select 'Engineering' from dual union all
+select 'Engineering' from dual union all
+select 'Legal' from dual union all
+select 'Legal' from dual union all
+select 'Services' from dual union all
+select 'Product Management' from dual union all
+select 'Human Resources' from dual union all
+select 'Business Development' from dual union all
+select 'Support' from dual union all
+select 'Sales' from dual union all
+select 'Legal' from dual union all
+select 'Product Management' from dual union all
+select 'Accounting' from dual union all
+select 'Legal' from dual union all
+select 'Human Resources' from dual union all
+select 'Training' from dual union all
+select 'Engineering' from dual union all
+select 'Engineering' from dual union all
+select 'Business Development' from dual union all
+select 'Services' from dual union all
+select 'Product Management' from dual union all
+select 'Product Management' from dual union all
+select 'Sales' from dual union all
+select 'Product Management' from dual union all
+select 'Product Management' from dual union all
+select 'Research and Development' from dual union all
+select 'Accounting' from dual union all
+select 'Product Management' from dual union all
+select 'Human Resources' from dual union all
+select 'Training' from dual union all
+select 'Sales' from dual union all
+select 'Marketing' from dual union all
+select 'Support' from dual union all
+select 'Legal' from dual union all
+select 'Human Resources' from dual union all
+select 'Legal' from dual union all
+select 'Training' from dual union all
+select 'Accounting' from dual union all
+select 'Engineering' from dual union all
+select 'Legal' from dual union all
+select 'Product Management' from dual union all
+select 'Support' from dual union all
+select 'Research and Development' from dual union all
+select 'Accounting' from dual union all
+select 'Research and Development' from dual union all
+select 'Legal' from dual union all
+select 'Services' from dual union all
+select 'Legal' from dual union all
+select 'Accounting' from dual union all
+select 'Legal' from dual union all
+select 'Support' from dual union all
+select 'Engineering' from dual union all
+select 'Product Management' from dual union all
+select 'Sales' from dual union all
+select 'Human Resources' from dual union all
+select 'Support' from dual union all
+select 'Business Development' from dual union all
+select 'Sales' from dual union all
+select 'Research and Development' from dual union all
+select 'Sales' from dual union all
+select 'Services' from dual union all
+select 'Sales' from dual union all
+select 'Support' from dual union all
+select 'Human Resources' from dual union all
+select 'Legal' from dual union all
+select 'Human Resources' from dual union all
+select 'Sales' from dual union all
+select 'Sales' from dual union all
+select 'Marketing' from dual union all
+select 'Accounting' from dual union all
+select 'Engineering' from dual union all
+select 'Product Management' from dual;
+
+
+insert into EMPLOYEES (EMPLOYEE_uin, department_id, EMPLOYEE_NAME, EMPLOYEE_lastname, EMPLOYEE_PHONE, EMPLOYEE_BIRTHDATE, EMPLOYMENT_DATE) 
+select '836126726263', 96, 'Zola', 'Tipping', '401 888 1629', '05/26/1987', '05/03/2010' from dual union all
+select '318884668268', 33, 'Fanni', 'Terry', '304 367 0535', '04/26/1987', '11/23/2015' from dual union all
+select '206748380823', 91, 'Alejoa', 'Bonner', '529 971 9961', '11/15/1995', '04/22/2004' from dual union all
+select '666520488273', 24, 'Brod', 'Kerans', '187 923 6317', '09/14/2000', '04/11/2019' from dual union all
+select '257500493114', 22, 'Ignacio', 'Stollsteiner', '144 352 2570', '03/30/1989', '07/28/2014' from dual union all
+select '585535233667', 25, 'Gui', 'Vamplew', '454 157 6146', '09/20/1992', '04/10/2013' from dual union all
+select '585596900187', 85, 'Amanda', 'Vercruysse', '650 830 6991', '11/13/1997', '10/21/2014' from dual union all
+select '724555874186', 34, 'Gena', 'Kruszelnicki', '879 817 7749', '04/18/1990', '11/12/2009' from dual union all
+select '850839596001', 5, 'Zahara', 'Romanini', '564 538 4912', '12/24/1995', '05/17/2015' from dual union all
+select '419580577247', 4, 'Arlin', 'Tumelty', '871 268 0293', '07/06/2004', '10/22/2010' from dual union all
+select '836820871639', 74, 'Jewel', 'Wyllcocks', '478 812 1726', '12/26/2004', '06/03/2017' from dual union all
+select '563856056258', 84, 'Avery', 'Diffley', '858 278 7799', '07/18/1984', '12/09/2013' from dual union all
+select '138109028845', 43, 'Orelie', 'Chalmers', '640 969 2613', '09/06/1982', '03/24/2007' from dual union all
+select '142763431457', 63, 'Colas', 'Huitson', '895 626 6738', '10/06/1982', '04/19/2017' from dual union all
+select '798169030855', 20, 'Keen', 'La Croce', '627 559 1753', '03/03/1990', '05/26/2009' from dual union all
+select '701788551156', 6, 'Carine', 'Parham', '848 434 6114', '05/02/1986', '07/18/2010' from dual union all
+select '678922816347', 28, 'Stanly', 'Hinks', '933 996 9590', '02/27/1992', '09/30/2006' from dual union all
+select '762787467284', 27, 'Elianora', 'Ennor', '485 640 3715', '04/11/1980', '10/21/2004' from dual union all
+select '609515770404', 6, 'Jecho', 'Arondel', '505 476 6077', '10/16/1982', '03/17/2011' from dual union all
+select '464380652400', 91, 'Mair', 'Rickford', '305 465 2441', '10/03/1989', '12/17/2005' from dual union all
+select '848771022082', 55, 'Ilka', 'Battaille', '784 163 1960', '04/10/2004', '11/15/2021' from dual union all
+select '374429677165', 96, 'Enrichetta', 'Palmar', '636 347 2594', '12/14/1983', '12/02/2004' from dual union all
+select '605792728350', 70, 'Melamie', 'Yeskin', '979 138 7518', '01/21/1999', '10/22/2007' from dual union all
+select '922803273380', 29, 'Geno', 'Linggood', '907 738 9933', '06/25/2004', '02/14/2010' from dual union all
+select '117465589163', 26, 'Ariella', 'Tarling', '170 795 6637', '01/18/1980', '02/16/2004' from dual union all
+select '524582752899', 54, 'Beilul', 'Breakspear', '420 856 9796', '10/06/1997', '07/11/2015' from dual union all
+select '486736130940', 48, 'Glennie', 'Halesworth', '342 620 7617', '07/22/2001', '12/06/2017' from dual union all
+select '914135781887', 99, 'Franky', 'Kadwallider', '800 876 8081', '11/20/1992', '03/26/2020' from dual union all
+select '205641468525', 67, 'Faustina', 'Roncelli', '953 344 0568', '06/03/2001', '08/02/2009' from dual union all
+select '621765799626', 78, 'Willow', 'Lowing', '605 373 0350', '06/20/2002', '11/24/2013' from dual union all
+select '437456717023', 82, 'Jeanie', 'Pentland', '306 349 3134', '09/05/1999', '01/25/2007' from dual union all
+select '687213927547', 81, 'Franni', 'Longstreet', '744 923 5036', '01/23/1994', '04/12/2016' from dual union all
+select '781365223732', 33, 'Gabriela', 'Willman', '824 244 8610', '03/29/1988', '10/23/2008' from dual union all
+select '944660475556', 43, 'Delcine', 'Forber', '185 841 7582', '06/16/1995', '03/01/2015' from dual union all
+select '116758741563', 87, 'Minna', 'Kubica', '905 887 6273', '03/06/1993', '11/17/2011' from dual union all
+select '566295511070', 41, 'Courtenay', 'Sorey', '452 605 4047', '01/25/2003', '03/29/2004' from dual union all
+select '831955348198', 23, 'Jamey', 'Burberye', '433 207 2565', '06/28/2002', '02/10/2019' from dual union all
+select '812141213172', 77, 'Zachariah', 'Pendrey', '514 570 6672', '02/24/1984', '12/31/2007' from dual union all
+select '437804913409', 64, 'Liesa', 'McElmurray', '624 304 0308', '05/21/1981', '05/23/2009' from dual union all
+select '747792355217', 77, 'Ethyl', 'Pulford', '242 724 2795', '09/15/2001', '07/11/2008' from dual union all
+select '104421676097', 40, 'Jerrylee', 'Gallaccio', '910 910 0195', '01/06/1999', '08/25/2014' from dual union all
+select '546164656545', 91, 'Violante', 'Richie', '224 652 5650', '08/19/1998', '02/08/2018' from dual union all
+select '107368883602', 21, 'Jerrilee', 'Fassan', '221 345 8463', '12/17/1991', '02/09/2008' from dual union all
+select '349482222704', 32, 'Janela', 'Chalke', '689 264 8129', '08/13/1989', '09/23/2019' from dual union all
+select '722914688566', 40, 'Augusto', 'Janew', '311 118 6054', '05/13/2004', '07/17/2004' from dual union all
+select '172703040523', 24, 'Tabbitha', 'Dalgarno', '890 221 4552', '05/13/1990', '12/16/2019' from dual union all
+select '500250597242', 63, 'Consolata', 'Kittredge', '865 883 8864', '12/27/1986', '01/15/2012' from dual union all
+select '707942148287', 66, 'Leontine', 'Bradnam', '285 949 9179', '10/22/1995', '08/31/2014' from dual union all
+select '490903929980', 98, 'Ozzy', 'Castletine', '521 411 4210', '02/16/1999', '02/01/2010' from dual union all
+select '459420932745', 25, 'Karmen', 'Meake', '490 875 1353', '11/23/1987', '01/09/2021' from dual union all
+select '926521251534', 95, 'Griswold', 'Tritten', '931 313 9551', '05/08/1989', '10/10/2016' from dual union all
+select '539284518281', 93, 'Jay', 'Escot', '170 804 9245', '10/16/1982', '09/09/2019' from dual union all
+select '701025156034', 51, 'Valli', 'Lembcke', '904 123 7974', '11/28/1987', '05/13/2017' from dual union all
+select '981122542391', 15, 'Brannon', 'Escott', '472 972 7319', '10/19/1995', '07/19/2010' from dual union all
+select '243320681810', 10, 'Calypso', 'Wrigglesworth', '795 987 4104', '10/21/1999', '10/07/2013' from dual union all
+select '414515957093', 81, 'Morey', 'Veillard', '399 228 6100', '10/02/1986', '06/01/2011' from dual union all
+select '360481641077', 61, 'Jermayne', 'Blackstock', '243 865 5478', '01/27/1994', '01/20/2009' from dual union all
+select '218167635675', 9, 'Meara', 'Maskelyne', '304 602 0852', '01/01/1999', '08/22/2013' from dual union all
+select '747099712654', 40, 'Mozelle', 'Upcott', '591 209 2993', '02/06/2002', '05/01/2008' from dual union all
+select '994979195726', 41, 'Jillana', 'Raitie', '606 769 6093', '09/29/1996', '01/25/2018' from dual union all
+select '802390686666', 80, 'Haskel', 'Garmanson', '289 478 5575', '10/18/2004', '07/05/2013' from dual union all
+select '801248965695', 55, 'Leanora', 'Burnhard', '712 949 8306', '05/27/1988', '01/31/2005' from dual union all
+select '231240472446', 22, 'Jamal', 'Arrighi', '291 260 3419', '09/01/1999', '06/03/2017' from dual union all
+select '160606837215', 71, 'Valdemar', 'Upward', '808 210 5357', '04/02/1986', '05/24/2018' from dual union all
+select '678522149770', 13, 'Sebastiano', 'Minter', '822 274 0095', '06/03/1993', '11/15/2011' from dual union all
+select '521799503648', 86, 'Arlene', 'Paulsen', '777 629 5279', '07/13/2001', '10/04/2010' from dual union all
+select '876538163597', 8, 'Dallas', 'O''Fihillie', '316 473 5322', '12/16/2001', '07/12/2019' from dual union all
+select '602279205918', 6, 'Beau', 'Shildrick', '517 208 9685', '10/07/2002', '09/16/2010' from dual union all
+select '166389893429', 25, 'Stillmann', 'Bellini', '537 454 8054', '03/15/2001', '06/14/2014' from dual union all
+select '508597617104', 21, 'Claudius', 'Skryne', '928 978 4389', '08/31/2001', '04/14/2016' from dual union all
+select '229615222515', 11, 'Kaitlyn', 'Pritchard', '412 892 7155', '06/12/1983', '08/16/2020' from dual union all
+select '788333840141', 77, 'Lauraine', 'Rimington', '308 990 8784', '03/02/1996', '11/04/2013' from dual union all
+select '444306784412', 68, 'Babs', 'Ellcock', '133 539 2435', '03/19/1998', '10/20/2009' from dual union all
+select '259962358092', 98, 'Bobbie', 'Maass', '828 682 7854', '04/19/1996', '01/06/2013' from dual union all
+select '725200887311', 40, 'Kally', 'Pembry', '665 367 2769', '10/21/1981', '08/28/2017' from dual union all
+select '122276530740', 7, 'Osbourn', 'Huckabe', '767 950 4843', '06/26/1991', '03/29/2006' from dual union all
+select '917989087680', 74, 'Aloise', 'Boij', '274 889 9456', '08/31/1998', '02/07/2006' from dual union all
+select '497295037718', 21, 'Shellysheldon', 'Razzell', '989 564 9234', '10/03/1983', '01/24/2014' from dual union all
+select '149499007970', 72, 'Bennett', 'Upsale', '744 811 4103', '12/31/1986', '06/01/2014' from dual union all
+select '397874765837', 28, 'Libby', 'Elleton', '126 918 7354', '11/27/1991', '04/30/2021' from dual union all
+select '910119593493', 37, 'Bidget', 'Confait', '783 182 0894', '03/05/1983', '03/30/2005' from dual union all
+select '857936744018', 46, 'Franklin', 'Brindley', '539 760 6058', '05/19/1997', '12/23/2011' from dual union all
+select '355139738813', 6, 'Legra', 'Hotton', '888 861 7956', '06/17/1997', '09/10/2013' from dual union all
+select '893930154312', 2, 'Vallie', 'Revel', '791 642 7984', '06/14/1994', '10/29/2010' from dual union all
+select '834933817992', 13, 'Daffy', 'Bakey', '228 161 9788', '01/04/1983', '01/10/2011' from dual union all
+select '381855731416', 56, 'Berta', 'Knuckles', '170 177 2605', '10/14/1986', '11/12/2005' from dual union all
+select '601535625959', 92, 'Shaylah', 'Tomovic', '397 137 8466', '05/11/1980', '12/06/2007' from dual union all
+select '716335640022', 12, 'Shana', 'Scurfield', '364 798 4787', '01/11/2002', '01/17/2016' from dual union all
+select '312757471946', 57, 'Issy', 'Killoran', '817 741 1741', '11/26/1995', '01/31/2009' from dual union all
+select '967456620528', 82, 'Michail', 'Keedwell', '988 999 7566', '03/02/1981', '11/24/2005' from dual union all
+select '181375168637', 41, 'Keeley', 'Matussow', '107 994 1482', '06/28/2000', '10/23/2016' from dual union all
+select '744999377700', 13, 'Gwendolen', 'Simoes', '257 212 1459', '07/26/2003', '09/08/2004' from dual union all
+select '728523570796', 62, 'Lorri', 'Hemphill', '118 673 4250', '12/06/1981', '06/23/2009' from dual union all
+select '894296573541', 52, 'Gipsy', 'Crepel', '230 769 8132', '08/12/1983', '07/25/2009' from dual union all
+select '915233644985', 63, 'Tatiania', 'Noon', '401 391 1735', '05/06/1996', '02/07/2009' from dual union all
+select '316694212782', 51, 'Lars', 'O''Carmody', '225 701 2469', '11/14/1999', '06/08/2013' from dual union all
+select '609056349523', 97, 'Davie', 'Taffrey', '763 919 9791', '01/28/2000', '12/06/2016' from dual union all
+select '192749235709', 85, 'Sasha', 'Sowthcote', '803 185 0021', '01/28/1999', '01/30/2018' from dual union all
+select '879911537566', 19, 'Eve', 'Blacktin', '911 539 2198', '07/23/1983', '03/27/2004' from dual union all
+select '881865395193', 76, 'Woodrow', 'Teresse', '792 223 1616', '04/23/2002', '05/30/2008' from dual;
